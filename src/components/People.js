03511019be5193eac28a28576cbf3fc1cf3a1dcc
Dirk Dobson <dirkvancouver@live.com>
2018-11-30 12:36:06 -0700

    pictures shows

--- src/components/People.js
+++ src/components/People.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import axios from 'axios';
+import {
+  Grid,
+  Col,
+  Thumbnail,
+} from 'react-bootstrap';
 
 const key = process.env.REACT_APP_API_KEY
 const BASE_URL = `https://api.themoviedb.org/3/trending/person/week?api_key=${key}`
@@ -7,7 +12,7 @@ const BASE_URL = `https://api.themoviedb.org/3/trending/person/week?api_key=${ke
 class People extends React.Component {
 
   state = {
-    data: {}
+    data: { results: [] }
   }
 
   componentDidMount() {
@@ -19,8 +24,19 @@ class People extends React.Component {
   }
 
   render() {
+    const { results } = this.state.data
     return (
       <>
+      <Grid>
+        { results.map(r =>
+        <Col xs={6} md={4}>
+          <Thumbnail src={`https://image.tmdb.org/t/p/w500${r.profile_path}`} alt="242x200 trending person">
+            <h3>{r.name}</h3>
+            <p>{r.known_for_department}</p>
+          </Thumbnail>
+        </Col>
+        )}
+    </Grid>
       </>
     )
   }
