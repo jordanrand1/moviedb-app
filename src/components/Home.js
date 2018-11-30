7dec76e687734edb32e7901d02db39c1287c2eb2
Jordan Rand <jorrand@gmail.com>
2018-11-30 10:34:09 -0700

    got data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
-const Home = () => (
-  <>
-  </>
-)
+const key = process.env.REACT_APP_API_KEY
+const BASE_URL = `https://api.themoviedb.org/3/trending/all/week?api_key=${key}`
+
+class Home extends React.Component {
+
+  state = {
+    data: {}
+  }
+
+  componentDidMount() {
+    axios.get(BASE_URL)
+      .then( res => this.setState({
+        data: res.data
+      })
+    )
+  }
+
+  render() {
+    return (
+      <>
+      </>
+    )
+  }
+}
 
 export default Home;
