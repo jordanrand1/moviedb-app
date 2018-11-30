51a75e2a82d802b9c93bcb52ccf1f38ebfbd288b
Dirk Dobson <dirkvancouver@live.com>
2018-11-30 11:24:21 -0700

    data shows people

--- src/components/People.js
+++ src/components/People.js
@@ -2,21 +2,21 @@ import React from 'react';
 import axios from 'axios';
 
 const key = process.env.REACT_APP_API_KEY
-const id = 1
+const BASE_URL = `https://api.themoviedb.org/3/trending/person/week?api_key=${key}`
 
 class People extends React.Component {
 
-  // state = {
-  //   data: {}
-  // }
+  state = {
+    data: {}
+  }
 
-  // componentDidMount() {
-  //   axios.get(BASE_URL)
-  //     .then( res => this.setState({
-  //       data: res.data
-  //     })
-  //   )
-  // }
+  componentDidMount() {
+    axios.get(BASE_URL)
+      .then( res => this.setState({
+        data: res.data
+      })
+    )
+  }
 
   render() {
     return (
