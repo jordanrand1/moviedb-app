b485e8295aa796855bacf1bcfbf9b001543cf042
tchoutdara <monkeymilitia444@gmail.com>
2018-11-30 12:38:31 -0700

    displaying data on tv show

--- src/components/TvShow.js
+++ src/components/TvShow.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {
+  Grid,
+  Col,
+  Thumbnail,
+  Row,
+ } from 'react-bootstrap';
 
 const key = process.env.REACT_APP_API_KEY
-const BASE_URL = 'https://api.themoviedb.org/3/trending/all/day?api_key=${key}'
+const BASE_URL = `https://api.themoviedb.org/3/trending/tv/week?api_key=${key}`
 
 class TvShow extends Component {
   state = {
@@ -16,11 +22,29 @@ class TvShow extends Component {
       })
     )
   }
+
+  show = () => {
+  const { results } = this.state.data
+    return (
+    <Grid>
+          { results.map(r =>
+          <Col xs={6} md={4}>
+            <Thumbnail src={`https://image.tmdb.org/t/p/w500${r.profile_path}`} alt="242x200 trending person">
+              <h3>{r.name}</h3>
+            </Thumbnail>
+          </Col>
+          )}
+      </Grid>
+    )
+  }
+  
   
   render() {
     return (
         <>
-          <h1>Testing Tv Shows</h1>
+        <Grid style={{padding: '0px', margin: '0px', width: '100%'}}>
+            {this.show()}
+        </Grid>
         </>
     );
   }
