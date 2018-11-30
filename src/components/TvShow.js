c687d2337bd04b846445a87612bba8efc8fbcbca
tchoutdara <monkeymilitia444@gmail.com>
2018-11-30 10:53:02 -0700

    tvshow fix

--- src/components/TvShow.js
+++ src/components/TvShow.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const key = process.env.REACT_APP_API_KEY
+const BASE_URL = 'https://api.themoviedb.org/3/trending/all/day?api_key=${key}'
 
 class TvShow extends Component {
+  state = {
+    data: {}
+  }
+
+  componentDidMount() {
+    axios.get(BASE_URL)
+      .then( res => this.setState({
+        data: res.data
+      })
+    )
+  }
+  
   render() {
     return (
-        <div>
-          <p>TV Show</p>
-        </div>
+        <>
+          <h1>Testing Tv Shows</h1>
+        </>
     );
   }
 }
