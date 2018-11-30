1f2eab02b5740df94701dcedd2270046e04b09bb
tchoutdara <monkeymilitia444@gmail.com>
2018-11-30 10:04:22 -0700

    add tvshow component

---
+++ src/components/TvShow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TvShow extends Component {
+  render() {
+    return (
+        <div>
+          <p>Testing</p>
+        </div>
+    );
+  }
+}
+
+export default TvShow;
