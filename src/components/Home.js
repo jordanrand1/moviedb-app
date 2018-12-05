d146d199ac68e03ccd981215ff9420cb4813548b
Jordan Rand <jorrand@gmail.com>
2018-12-04 20:34:06 -0700

    changed slide titles

--- src/components/Home.js
+++ src/components/Home.js
@@ -121,8 +121,8 @@ class Home extends React.Component {
             </Row>
           </Grid>
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>Top Movies</h3>
+            <p></p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -132,8 +132,8 @@ class Home extends React.Component {
             </Row>
           </Grid>
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>Top Shows</h3>
+            <p></p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -143,8 +143,8 @@ class Home extends React.Component {
             </Row>
           </Grid>
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            <h3>Top Actors</h3>
+            <p></p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>;
